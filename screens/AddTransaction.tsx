42b240a12ec64172796a9c9b223444a58c4d5821
blacktornado2 <blacktornado2108@gmail.com>
2025-04-23 16:53:17 +0530

    UI fixes

--- screens/AddTransaction.tsx
+++ screens/AddTransaction.tsx
@@ -15,26 +15,27 @@ const AddTransactionScreen = () => {
 
   const router = useRouter();
 
+  // TODO: make all the variables here, that are used in the form
   const [amount, setAmount] = useState(0);
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
   const [date, setDate] = useState(format(new Date(), 'MMMM dd, yyyy'));
 
   return (
-    <View className='px-8 mt-16'>
+    <View className='px-8 pt-16 bg-gray-100'>
       <Text className='text-4xl pb-5 font-bold'>{ADD_TRANSACTION}</Text>
       <View className='my-3'>
         <View className='mb-2 flex flex-row'>
           <FontAwesome5 name="money-bill" size={24} color="green" />
           <Text className='mx-2 text-xl'>{AMOUNT}</Text>
         </View>
-        <TextInput className='border border-gray-300 py-2 px-4 text-xl flex' placeholder='&#8377; 0' />
+        <TextInput className='border border-gray-300 py-2 px-4 text-xl flex bg-white' placeholder='&#8377; 0' />
       </View>
       <View className='my-3'>
         <View className='mb-2 flex flex-row'>
           <MaterialIcons name="category" size={24} color="blue" />
           <Text className='mx-2 text-xl'>{CATEGORY}</Text>
         </View>
-        <TextInput className='border border-gray-300 py-2 px-4 text-xl flex' placeholder='category' />
+        <TextInput className='border border-gray-300 py-2 px-4 text-xl flex bg-white' placeholder='category' />
       </View>
       <View className='my-3'>
         <View className='mb-2 flex flex-row'>
@@ -42,7 +43,7 @@ const AddTransactionScreen = () => {
           <Text className='mx-2 text-xl'>{DESCRIPTION}</Text>
         </View>
         <TextInput
-          className='border border-gray-300 py-2 px-4 text-lg h-36'
+          className='border border-gray-300 py-2 px-4 text-lg h-36 bg-white'
           multiline
           placeholder='Add Description about the transaction'
           numberOfLines={5}
@@ -50,7 +51,7 @@ const AddTransactionScreen = () => {
       </View>
       <View className='my-3'>
         <Text className='text-xl mb-2'>{DATE}</Text>
-        <TouchableOpacity onPress={() => setDatePickerVisibility(!isDatePickerVisible)} className='border border-gray-300 py-2 px-4 text-lg flex flex-row items-center' >
+        <TouchableOpacity onPress={() => setDatePickerVisibility(!isDatePickerVisible)} className='border border-gray-300 py-2 px-4 text-lg flex flex-row items-center bg-white' >
           <AntDesign name="calendar" size={24} color="green" />
           <Text className='text-lg mx-4'>{date}</Text>
         </TouchableOpacity>
