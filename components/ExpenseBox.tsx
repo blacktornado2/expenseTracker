1d659e6f5092452e7d5e4c08a580976c5a429d4c
roshanyadav1793 <roshanyadav1793@gmail.com>
2025-04-27 23:11:50 +0530

    dashboard screen

---
+++ components/ExpenseBox.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ExpenseBox = ({
+    title,
+    amount,
+    className,
+  }: {
+    title: string
+    amount: number
+    className?: string
+  }) => {
+  return (
+    <View className={`p-4 rounded-2xl ${className} shadow-md shadow-black/20`}>
+        <Text className='text-base font-semibold'>{title}</Text>
+        <Text className='text-slate-700'>${amount}</Text>
+    </View>
+  )
+}
+
+export default ExpenseBox;
---
+++ screens/Dashboard.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Text, SafeAreaView, View, ScrollView } from 'react-native';
+import ExpenseBox from '@/components/ExpenseBox';
+import PieChart from 'react-native-pie-chart';
+
+const Dashboard = () => {
+  const categoryData = [
+    { label: 'Food', value: 40, color: '#FF9384' },
+    { label: 'Transport', value: 30, color: '#36A2EB' },
+    { label: 'Shopping', value: 20, color: '#FFCE56' },
+    { label: 'Other', value: 10, color: '#4BC0C0' },
+  ];
+
+  const sliceColor = categoryData.map(item => item.color);
+
+  const TransactionRow = ({ date, title, amount, isLast }: { date: string; title: string; amount: number; isLast?: boolean }) => {
+    const [month, day] = date.split(' ');
+  
+    return (
+      <View className='px-5 py-4'>
+        <View className='flex-row justify-between items-center'>
+          {/* Left side: Date + Title */}
+          <View className='flex-row items-center'>
+            <View className='items-center'>
+              <Text className='text-lg font-bold'>{day}</Text>
+              <Text className='text-xs text-gray-500'>{month}</Text>
+            </View>
+            <Text className='ml-8 text-lg font-semibold'>{title}</Text>
+          </View>
+  
+          {/* Right side: Amount */}
+          <Text className='text-lg font-semibold'>{amount}</Text>
+        </View>
+  
+        {/* Divider */}
+        {!isLast && (
+          <View className='mt-4 mx-2 h-px bg-gray-200' />
+        )}
+      </View>
+    );
+  };
+  
+
+  return (
+    <SafeAreaView className='flex-1 bg-gray-50'>
+      <ScrollView contentContainerStyle={{ paddingBottom: 30 }}>
+        <View className='mx-6 mt-15'>
+          <Text className='text-4xl text-black font-bold'>Dashboard</Text>
+          <View className='flex-row justify-between mt-6'>
+            <ExpenseBox title='Total Income:' amount={5000} className="bg-green-400" />
+            <ExpenseBox title='Total Expense:' amount={3000} className="bg-red-300" />
+            <ExpenseBox title='Balance:' amount={2000} className="bg-gray-100" />
+          </View>
+
+          <View className='mt-8 px-5 bg-white items-center py-8 rounded-2xl'>
+            <Text className='text-2xl font-semibold mb-4'>Category Breakdown</Text>
+            <PieChart
+              widthAndHeight={160}
+              series={categoryData}
+              sliceColor={sliceColor}
+              coverRadius={0.6}
+              coverFill={'#FFF'}
+            />
+          </View>
+
+          <Text className='text-2xl font-semibold mt-8 mb-4'>Recent Transactions</Text>
+          <View className='bg-white rounded-2xl'>
+            <TransactionRow date='Apr 20' title='Groceries' amount={100} />
+            <TransactionRow date='Apr 21' title='Shopping' amount={300} />
+            <TransactionRow date='Apr 22' title='Bank transfer' amount={1000} isLast={true} />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Dashboard;
