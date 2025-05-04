6522bb1c1a164f4e560ec00ad9c540c6d8eabf9f
blacktornado2 <blacktornado2108@gmail.com>
2025-05-04 10:34:43 +0530

    typo fixed

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const MONGODB_CONNECTION_URI_STAGING = process.env.MONGODB_CONNECTION_URI_TEST;
+const MONGODB_CONNECTION_URI_TEST = process.env.MONGODB_CONNECTION_URI_TEST;
 
-connectDB(MONGODB_CONNECTION_URI_STAGING);
+connectDB(MONGODB_CONNECTION_URI_TEST);
 
 const PORT = process.env.PORT || 3000;
 
