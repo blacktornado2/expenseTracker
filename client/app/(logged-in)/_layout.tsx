a623b8a7553418e61cdd41f468f004d2accae4c5
blacktornado2 <blacktornado2108@gmail.com>
2025-07-16 14:40:37 +0530

    token expiry added

--- client/app/(logged-in)/_layout.tsx
+++ client/app/(logged-in)/_layout.tsx
@@ -1,27 +1,37 @@
-import { Redirect, Stack, useRouter } from 'expo-router';
+import React from 'react';
+import { Stack, useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 export default function AuthLayout() {
     const router = useRouter();
     const [isLoading, setIsLoading] = useState(true);
 
-    // const [token, setToken] = useState(async () => await AsyncStorage.getItem('JWT_TOKEN'));
-
     useEffect(() => {
         const checkAuth = async () => {
             try {
                 const token = await AsyncStorage.getItem('JWT_TOKEN');
-                console.log("token", token);
+                console.log("token: ", token);
+
                 if (!token) {
                     router.replace('/login');
+                    return;
                 }
+
+                const { exp } = jwtDecode(token) as { exp: number };
+                const isTokenExpired = Date.now() > (exp * 1000);
+
+                if (isTokenExpired) {
+                    router.replace('/login');
+                    return;
+                }
+
             } catch (error) {
                 console.error(error);
                 router.replace('/login');
             } finally {
                 setIsLoading(false)
-
             }
         }
         checkAuth();
