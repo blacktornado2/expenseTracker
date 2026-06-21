342c2d7372aff7b1d4efd2c50061646955f75d75
blacktornado2 <blacktornado2108@gmail.com>
2026-06-21 11:07:22 +0530

    feat: handle CREATE_TRANSACTION_SUCCESS/FAILURE in transaction reducer

--- client/redux/reducers/transaction.reducer.ts
+++ client/redux/reducers/transaction.reducer.ts
@@ -1,13 +1,18 @@
-import {GET_TRANSACTIONS_SUCCESS} from '../actions/action.types'
+import { GET_TRANSACTIONS_SUCCESS, CREATE_TRANSACTION_SUCCESS, CREATE_TRANSACTION_FAILURE } from '../actions/action.types'
 
 const initialState = {
     transactions: [],
+    createError: null,
 };
 
 const transactionReducer = (state = initialState, action: any) => {
     switch(action.type) {
         case GET_TRANSACTIONS_SUCCESS:
             return {...state, transactions: action.payload}
+        case CREATE_TRANSACTION_SUCCESS:
+            return {...state, transactions: [action.payload, ...state.transactions], createError: null}
+        case CREATE_TRANSACTION_FAILURE:
+            return {...state, createError: action.payload}
         default:
             return state;
     }
