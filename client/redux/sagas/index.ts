b0ac6f3e90b84b1cfe16aeff392c4a643998f2f8
ankit-betterworks <ankit.bhardwaj@betterworks.com>
2026-06-22 09:52:12 +0530

    feat(client): budget service + sagas wired into root saga

--- client/redux/sagas/index.ts
+++ client/redux/sagas/index.ts
@@ -1,8 +1,10 @@
 import { fork } from "redux-saga/effects";
 import { watchFetchUser } from "./user.sagas"; // Import your user saga
 import {watchTransactionsRequests} from './transaction.sagas';
+import { watchBudgetRequests } from './budget.sagas';
 
 export function* rootSaga() {
   yield fork(watchFetchUser);
   yield fork(watchTransactionsRequests);
+  yield fork(watchBudgetRequests);
 }
